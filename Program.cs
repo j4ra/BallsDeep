639f3621356eeae92115e242263e31831f7a83c4
Jaroslav Nejedly <jardanejedly@hotmail.com>
2017-04-10 19:54:41 +0200

    Oprava bugů. Výpočet celého příkladu :100:  :+1:  :-)

--- Program.cs
+++ Program.cs
@@ -17,27 +17,33 @@ namespace BallsDeep
         static Ball[] balls = new Ball[2];
 
         static double distance;
+        static double Q;
+        static double A;
+        static double U;
 
         static void Main(string[] args)
         {
             balls[0] = new Ball();
             balls[1] = new Ball();
-            Console.WriteLine("Prozraď mi poloměr první koule:");
-            balls[0].a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Prozraď mi potenciál první koule");
+            Console.WriteLine("Prozraď mi poloměr první koule [m]:");
+            balls[0].a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Prozraď mi potenciál první koule [V]:");
             balls[0].U = double.Parse(Console.ReadLine());
-            Console.WriteLine("Prozraď mi poloměr druhé koule:");
-            balls[1].a = int.Parse(Console.ReadLine());
-            Console.WriteLine("Prozraď mi potenciál druhé koule:");
+            Console.WriteLine("Prozraď mi poloměr druhé koule [m]:");
+            balls[1].a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Prozraď mi potenciál druhé koule [V]:");
             balls[1].U = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Prozraď mi vzdálenost koulí:");
+            Console.WriteLine("Prozraď mi vzdálenost koulí [m]:");
             distance = double.Parse(Console.ReadLine());
 
             Console.WriteLine("Počítám...");
             compute();
             Console.WriteLine("Sčítám...");
-            Console.WriteLine("Výsledek: " + sum());
+            Q = sum();
+            Koule();
+            Console.WriteLine("Celkový Q: " + Q);
+            Console.WriteLine("Celkový U: " + U);
             Console.ReadLine();
         }
 
@@ -54,7 +60,9 @@ namespace BallsDeep
             balls[0].d = new double[N];
             balls[1].d = new double[N];
             balls[0].q[0] = balls[0].U * balls[0].a * 4 * pi * epsilon0;
+            Console.WriteLine("Q0 = " + balls[0].q[0]);
             balls[1].q[0] = balls[1].U * balls[1].a * 4 * pi * epsilon0;
+            Console.WriteLine("Q0' = " + balls[1].q[0]);
             balls[0].d[0] = 0.0;
             balls[1].d[0] = distance;
             for(int i = 1; i < N; i++)
@@ -64,9 +72,14 @@ namespace BallsDeep
                 next(balls[0].a, balls[1].q[i - 1], balls[1].d[i - 1], out Q, out D);
                 balls[0].q[i] = Q;
                 balls[0].d[i] = D;
-                next(balls[1].a, balls[0].q[i - 1], distance - balls[0].d[i - 1], out Q, out D);
+                next(balls[1].a, balls[0].q[i - 1], Math.Abs(distance - balls[0].d[i - 1]), out Q, out D);
                 balls[1].q[i] = Q;
-                balls[1].d[i] = D;
+                balls[1].d[i] = distance - D;
+                if(i < 5)
+                {
+                    Console.WriteLine("Q" + i + " = " + balls[0].q[i]);
+                    Console.WriteLine("Q'" + i + " = " + balls[1].q[i]);
+                }
             }
         }
 
@@ -77,7 +90,7 @@ namespace BallsDeep
             for (int i = 0; i < N; i++)
             {
                 sum += balls[0].q[i] + balls[1].q[i];
-                if(Math.Abs(sum - pre_sum) <= precision)
+                if(i >= 50 && Math.Abs(sum - pre_sum) <= precision)
                 {
                     Console.WriteLine("Precision reached!");
                     break;
@@ -85,6 +98,15 @@ namespace BallsDeep
                 pre_sum = sum;
             }
             return sum;
+        }
+
+        static void Koule()
+        {
+            double V1 = 4.0 / 3.0 * pi * balls[0].a*balls[0].a*balls[0].a;
+            double V2 = 4.0 / 3.0 * pi * balls[1].a*balls[1].a*balls[1].a;
+            double V = V1 + V2;
+            A = Math.Pow(3.0/4.0/pi*V, 1.0/3.0);
+            U = 1.0 / 4.0 / pi / epsilon0 * Q / A;
         }
     }
 
